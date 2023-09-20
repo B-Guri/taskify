48f0239fd9b0afcb9a236ecf4682b7fb85d6f196
Bohdan <hursky73@gmail.com>
2023-09-21 00:45:37 +0300

    connect db and add user tble

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './transform.interceptor';
+
+console.log(process.env.NODE);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
+  const PORT = process.env.PORT || 3001;
+  await app.listen(PORT);
+  console.log(`App listening on port ${PORT}`);
 }
 bootstrap();
---
+++ src/user/user.entity.ts
@@ -0,0 +1,21 @@
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'user' })
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Exclude()
+  @Column()
+  password: string;
+}
